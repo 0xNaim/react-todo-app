e23f02e98276b7218dff17aa70d44f84f8ded60a
0xNaim <naimislam3545@gmail.com>
2021-02-14 22:08:15 +0600

    form validate function adden

--- src/components/controllers/filterController.jsx
+++ src/components/controllers/filterController.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import React from "react";
 import { Button, ButtonGroup } from "reactstrap";
 
-const FilterController = ({ handleFilter, handleColor }) => (
+const FilterController = ({ handleFilter }) => (
   <ButtonGroup>
     <Button onClick={() => handleFilter("all")}>All</Button>
     <Button onClick={() => handleFilter("running")}>Running</Button>
--- src/components/create-todo-form/index.jsx
+++ src/components/create-todo-form/index.jsx
@@ -6,6 +6,7 @@ class CreateTodoForm extends Component {
   state = {
     text: "",
     description: "",
+    errors: {},
   };
 
   handleChange = (event) => {
@@ -16,9 +17,30 @@ class CreateTodoForm extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    this.props.createTodo(this.state);
-    event.target.reset();
-    this.setState({ text: "", description: "" });
+    const { isValid, errors } = this.validate();
+
+    if (isValid) {
+      this.props.createTodo(this.state);
+      event.target.reset();
+      this.setState({ text: "", description: "" });
+    } else {
+      this.setState({ errors });
+    }
+  };
+
+  validate = () => {
+    const errors = {};
+    const { text, description } = this.state;
+    if (!text) {
+      errors.text = "Please provide task name";
+    }
+    if (!description) {
+      errors.description = "Please provide task description";
+    }
+    return {
+      errors,
+      isValid: Object.keys(errors).length === 0,
+    };
   };
 
   render() {
@@ -27,21 +49,39 @@ class CreateTodoForm extends Component {
         <FormGroup>
           <Label>Task Name</Label>
           <Input
+            className={
+              this.state.errors.text
+                ? "form-control is-invalid"
+                : "form-control"
+            }
             placeholder="Create your task"
             name="text"
             value={this.state.text}
             onChange={this.handleChange}
           />
+          {this.state.errors.text && (
+            <div className="invalid-feedback">{this.state.errors.text}</div>
+          )}
         </FormGroup>
         <FormGroup>
           <Label>Describe Task</Label>
           <Input
+            className={
+              this.state.errors.description
+                ? "form-control is-invalid"
+                : "form-control"
+            }
             type="textarea"
             placeholder="Describe your task"
             name="description"
             value={this.state.description}
             onChange={this.handleChange}
           />
+          {this.state.errors.description && (
+            <div className="invalid-feedback">
+              {this.state.errors.description}
+            </div>
+          )}
         </FormGroup>
         <Button type="submit">Create task</Button>
       </Form>
